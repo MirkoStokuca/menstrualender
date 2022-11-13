9934c9762433ba3fcf9793dbb15814e1dd12a6d9
vincent-clifford <vincent.clifford@me.com>
2022-11-13 12:19:52 +0100

    cleaned up stacked bar graph, waiting for database update

--- src/main/java/com/example/menstrualender/view/MensController.java
+++ src/main/java/com/example/menstrualender/view/MensController.java
@@ -12,10 +12,7 @@ import com.example.menstrualender.MensApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.StackedBarChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
@@ -25,6 +22,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -73,6 +71,71 @@ public class MensController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb){
         //Init pie chart
+        initPieChart();
+
+        //Init stacked bar chart
+        initStackedBarChart();
+        loadStackedBarChart();
+
+    }
+
+    /**
+     * Init stacked bar chart with empty data
+     */
+    private void initStackedBarChart(){
+        stackedBarChart.getXAxis().setOpacity(0); //hide x axis
+        stackedBarChart.getYAxis().setOpacity(0); //hide y axis
+    }
+
+    /**
+     * Load stored data in db into stacked bar chart
+     */
+    private void loadStackedBarChart(){
+
+        String [][] stringList = {
+                {"01.01.2022", "10", "4", "9"},
+                {"01.02.2022", "11", "5", "10"},
+                {"01.03.2022", "12", "4", "11"},
+                {"01.04.2022", "9", "3", "12"},
+                {"01.05.2022", "10", "6", "10"},
+                {"01.06.2022", "11", "7", "10"}
+        };
+
+        //create Series Instances
+        XYChart.Series series1= new XYChart.Series();
+        XYChart.Series series2= new XYChart.Series();
+        XYChart.Series series3= new XYChart.Series();
+
+        //fill series with placeholder values
+        for (int i = 12; i > stringList.length; i--) {
+            String placeholder = "Placeholder" + i;
+
+            series1.getData().add(new XYChart.Data(0,placeholder));
+            series2.getData().add(new XYChart.Data(0,placeholder));
+            series3.getData().add(new XYChart.Data(0,placeholder));
+        }
+
+        //add data to series
+        for (var cycle : stringList) {
+
+            String tempStartDate = cycle[0];
+            int tempBefore = Integer.parseInt(cycle[1]);
+            int tempDuring = Integer.parseInt(cycle[2]);
+            int tempAfter = Integer.parseInt(cycle[3]);
+
+            series1.getData().add(new XYChart.Data(tempBefore,tempStartDate));
+            series2.getData().add(new XYChart.Data(tempBefore,tempStartDate));
+            series3.getData().add(new XYChart.Data(tempBefore,tempStartDate));
+        }
+
+        //add series to stackedBarChart
+        stackedBarChart.getData().addAll(series1, series2, series3);
+    }
+
+    /**
+     * Init pie chart
+     */
+    private void initPieChart(){
         ObservableList<PieChart.Data>cycleChartData =
                 FXCollections.observableArrayList(
                         new PieChart.Data("", 15),
@@ -80,44 +143,9 @@ public class MensController implements Initializable {
         cycleGraph.setData(cycleChartData);
         cycleGraph.autosize();
         cycleGraph.setStartAngle(-100);
-
-        //Init stacked bar chart
-        XYChart.Series series1= new XYChart.Series(); //create Series Instance
-        series1.setName("Vor Blutung"); //display as legend
-        series1.getData().add(new XYChart.Data(10, "01.11.2022"));
-        series1.getData().add(new XYChart.Data(11, "01.12.2022"));
-        series1.getData().add(new XYChart.Data(12, "01.01.2023"));
-        series1.getData().add(new XYChart.Data(8, "01.02.2023"));
-        series1.getData().add(new XYChart.Data(9, "01.03.2023"));
-        series1.getData().add(new XYChart.Data(10, "01.04.2023"));
-        series1.getData().add(new XYChart.Data(13, "01.05.2023"));
-
-        XYChart.Series series2= new XYChart.Series(); //create Series Instance
-        series2.setName("Blutung"); //display as legend
-        series2.getData().add(new XYChart.Data(4, "01.11.2022"));
-        series2.getData().add(new XYChart.Data(5, "01.12.2022"));
-        series2.getData().add(new XYChart.Data(4, "01.01.2023"));
-        series2.getData().add(new XYChart.Data(6, "01.02.2023"));
-        series2.getData().add(new XYChart.Data(4, "01.03.2023"));
-        series2.getData().add(new XYChart.Data(4, "01.04.2023"));
-        series2.getData().add(new XYChart.Data(6, "01.05.2023"));
-
-        XYChart.Series series3= new XYChart.Series(); //create Series Instance
-        series3.setName("Nach Blutung"); //display as legend
-        series3.getData().add(new XYChart.Data(10,"01.11.2022"));
-        series3.getData().add(new XYChart.Data(11, "01.12.2022"));
-        series3.getData().add(new XYChart.Data(12, "01.01.2023"));
-        series3.getData().add(new XYChart.Data(8, "01.02.2023"));
-        series3.getData().add(new XYChart.Data(9, "01.03.2023"));
-        series3.getData().add(new XYChart.Data(10, "01.04.2023"));
-        series3.getData().add(new XYChart.Data(12, "01.05.2023"));
-
-
-
-        stackedBarChart.getData().addAll(series1, series2, series3);
-
-
     }
+
+
     @FXML
     public void loadData() {
         zyklus.readData();
@@ -181,7 +209,7 @@ public class MensController implements Initializable {
                         "\n\nAdd at least two dates\nto calulate the start\nof the next cycle.";
             } else {
                 do {
-                    message += LocalDate.parse(rs.getString("cyc_date_start")).format(DateUtil.formatter) + "\n";
+                    message += LocalDate.parse(rs.getString("cyc_date_start")).format(DateUtil.formatterLong) + "\n";
                 } while (rs.next());
             }
         } catch (SQLException e) {
